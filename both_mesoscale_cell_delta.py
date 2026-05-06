618cc46fc106543b3a967eafc0860b2c0ff6de69
indigo-fish <amelia.adcroft+github@gmail.com>
2026-05-06 14:40:24 -0400

    add nondimensionalized

--- both_mesoscale_cell_delta.py
+++ both_mesoscale_cell_delta.py
@@ -10,7 +10,9 @@ colors = ['blue', 'green']
 markers = ['o', 'v']
 linestyles = ['dashed', 'dotted']
 
-fig, ax = plt.subplots(figsize=(8, 5))
+fig, axes = plt.subplots(ncols=2, figsize=(12, 5), width_ratios=[1, 1])
+
+ax = axes[0]
 for i in range(2):
     ds = datasets[i]
     inv_deltax = ds['inv_deltax']
@@ -20,8 +22,26 @@ for i in range(2):
             color=colors[i], marker=markers[i],
             label=f'LES deficit: {labels[i]}',)
     ax.plot(inv_deltax, pred_delta_u, color=colors[i], linestyle=linestyles[i], linewidth=2.5, label=f'predicted in AIF: {labels[i]}')
-ax.legend()
+ax.plot([], [], color='turquoise', linestyle='dashdot', linewidth=2.5, label=f'predicted in AIF: both')
 ax.set_xlabel(r'$1/\Delta x$ (1/m)')
 ax.set_ylabel('Average upstream wind speed deficit (m/s)')
+
+ax.legend(loc='center right')
+
+ax = axes[1]
+u_inftys = [9.567571, 9.740854]
+for i in range(2):
+    ds = datasets[i]
+    u_infty = u_inftys[i]
+    inv_deltax = ds['inv_deltax']
+    mean_speeds = ds['actual'] / u_infty
+    pred_delta_u = ds['predicted'] / u_infty
+    ax.plot(inv_deltax, mean_speeds,
+            color=colors[i], marker=markers[i],
+            label=f'LES deficit: {labels[i]}',)
+    if i == 1:
+        ax.plot(inv_deltax, pred_delta_u, color='turquoise', linestyle='dashdot', linewidth=2.5, label=f'predicted in AIF: both')
+ax.set_xlabel(r'$1/\Delta x$ (1/m)')
+ax.set_ylabel('Average normalized upstream wind speed deficit')
 plt.tight_layout()
 plt.savefig('mesoscale_cell_delta.png', bbox_inches='tight', dpi=300)

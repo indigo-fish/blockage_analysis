f9f3381bfcbfff8a7127d2e0b365b99765811b6c
indigo-fish <amelia.adcroft+github@gmail.com>
2026-05-12 14:25:50 -0400

    annotate + fix legend placement

--- both_mesoscale_cell_delta.py
+++ both_mesoscale_cell_delta.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+plt.rcParams['font.size'] = 12
+
 neutral = pd.read_csv('grid_cell_wind_speed_delta_neutral.csv', index_col=0, header=0)
 stable = pd.read_csv('grid_cell_wind_speed_delta_stable.csv', index_col=0, header=0)
 datasets = [neutral, stable]
@@ -26,7 +28,18 @@ ax.plot([], [], color='turquoise', linestyle='dashdot', linewidth=2.5, label=f'p
 ax.set_xlabel(r'$1/\Delta x$ (1/m)')
 ax.set_ylabel('Average upstream wind speed deficit (m/s)')
 
-ax.legend(loc='center right')
+# ax.legend(loc='lower left')
+
+ax.set_title('Absolute wind speed deficit')
+
+ax.text(
+        0.02, 0.9, f'(a)',
+        transform=ax.transAxes,
+        fontsize=18,
+        fontweight='bold',
+        va='top',
+        ha='left'
+    )
 
 ax = axes[1]
 u_inftys = [9.567571, 9.740854]
@@ -43,5 +56,22 @@ for i in range(2):
         ax.plot(inv_deltax, pred_delta_u, color='turquoise', linestyle='dashdot', linewidth=2.5, label=f'predicted in AIF: both')
 ax.set_xlabel(r'$1/\Delta x$ (1/m)')
 ax.set_ylabel('Average normalized upstream wind speed deficit')
+
+ax.set_title('Nondimensionalized wind speed deficit')
+
+ax.text(
+        0.02, 0.9, f'(b)',
+        transform=ax.transAxes,
+        fontsize=18,
+        fontweight='bold',
+        va='top',
+        ha='left'
+    )
+
+ax.plot([], [], color='blue', linestyle='dashed', linewidth=2.5, label=r'predicted in AIF: N_WT $-$ N_NWT')
+ax.plot([], [], color='green', linestyle='dotted', linewidth=2.5, label=r'predicted in AIF: S_WT $-$ S_NWT')
+
+ax.legend(loc='lower left')
+
 plt.tight_layout()
 plt.savefig('mesoscale_cell_delta.png', bbox_inches='tight', dpi=300)

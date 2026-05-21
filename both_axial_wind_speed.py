c9c92768288259a7069e83934b1b79328cdbf247
indigo-fish <amelia.adcroft+github@gmail.com>
2026-05-21 16:30:40 -0400

    only one figure of induction zone with both stabilities

--- both_axial_wind_speed.py
+++ both_axial_wind_speed.py
@@ -9,7 +9,8 @@ neutral = pd.read_csv('axial_wind_speed_neutral.csv', index_col=0, header=0)
 stable = pd.read_csv('axial_wind_speed_stable.csv', index_col=0, header=0)
 datasets = [neutral, stable]
 labels = ['Neutral', 'Stable']
-
+colors = ['blue', 'green']
+linestyles = ['solid', 'dashed']
 
 # --- Secondary axis ---
 def meters_to_D(x):
@@ -20,9 +21,8 @@ def D_to_meters(x):
     return x * 127
 
 
-fig, axes = plt.subplots(ncols=2, sharey=True, figsize=(10, 5), width_ratios=[1, 1])
+fig, ax = plt.subplots(ncols=1, sharey=True, figsize=(6.5, 5))
 for i in range(2):
-    ax = axes[i]
     ds = datasets[i]
     mean = ds['mean']
     std = ds['std']
@@ -32,48 +32,45 @@ for i in range(2):
         x,
         mean - std,
         mean + std,
-        color='grey',
-        alpha=0.5,
-        label=f'±1 Std Dev'
+        color=colors[i],
+        alpha=0.2,
+        label=f'±1 Std Dev {labels[i]}'
     )
+
+for i in range(2):
+    ds = datasets[i]
+    mean = ds['mean']
+    std = ds['std']
+    x = ds['nx_dx']
 
     ax.plot(
         x,
         mean,
-        label=f'Mean',
-        color='black'
+        label=f'Mean {labels[i]}',
+        color=colors[i],
+        linestyle=linestyles[i],
     )
+ax.set_xticks(np.linspace(-2000, 0, 5))
+ax.set_xlabel('X (m)')
 
-    ax.vlines([0], -4.5, 1,
-              color='black', linestyle='dashed',
-              label='turbine position')
+ax.set_xlim(-2000, 0)
+ax.set_ylim(-2.5, 0.5)
 
-    ax.set_xlabel('X (m)')
+secax = ax.secondary_xaxis('top', functions=(meters_to_D, D_to_meters))
+secax.set_xlabel(r'X (Rotor Diameters, $D$)')
+secax.set_xticks(np.linspace(-16, 0, 5))
 
-    ax.set_title(labels[i])
+print("--- Upwind subset ---")
+upwind_subset = ds.loc[ds['nx_dx'] < 0]
+print(upwind_subset)
+cutoff = min(upwind_subset['mean']) / 10
+print(upwind_subset.loc[upwind_subset['mean'] == min(upwind_subset['mean'])])
+print(upwind_subset.loc[upwind_subset['mean'] < cutoff])
+print("Different from 0")
+print(ds.loc[ds['mean'] + ds['std'] / np.sqrt(258) > 0])
 
-    secax = ax.secondary_xaxis('top', functions=(meters_to_D, D_to_meters))
-    secax.set_xlabel(r'X (Rotor Diameters, $D$)')
-
-    print("--- Upwind subset ---")
-    upwind_subset = ds.loc[ds['nx_dx'] < 0]
-    print(upwind_subset)
-    cutoff = min(upwind_subset['mean']) / 10
-    print(upwind_subset.loc[upwind_subset['mean'] == min(upwind_subset['mean'])])
-    print(upwind_subset.loc[upwind_subset['mean'] < cutoff])
-    print("Different from 0")
-    print(ds.loc[ds['mean'] + ds['std'] / np.sqrt(258) > 0])
-
-    ax.text(
-        0.02, 0.98, f'({panel_labels[i]})',
-        transform=ax.transAxes,
-        fontsize=18,
-        fontweight='bold',
-        va='top',
-        ha='left'
-    )
-axes[0].set_ylabel('Wind Speed (m/s)')
-axes[0].legend(loc='lower right')
+ax.set_ylabel('Wind Speed (m/s)')
+ax.legend(loc='lower right')
 
 plt.tight_layout()
 plt.savefig('axial_wind_speed_delta.png', bbox_inches='tight', dpi=300)
